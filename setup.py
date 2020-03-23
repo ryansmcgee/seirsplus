760424fe1a16cada91fa1e592de1cf6a0122f523
Ryan McGee <ryan@Misty.local>
2020-03-23 03:37:41 -0700

    Packaging

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     packages=setuptools.find_packages(),
+    name="SEIRS-network-model",
     version='0.0.1',
     description='Models for SEIRS epidemic dynamics on networks and with interventions, such as testing, contact tracing, and social distancing.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ryansmcgee/SEIRS-network-model",
-    name="ryansmcgee",
     author='Ryan Seamus McGee',
     author_email='ryansmcgee@gmail.com',
     license='MIT',
