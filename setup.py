a82ff565ca9a52fdf2e2ea442425eac7abf58fb2
Ryan McGee <ryan@Misty.local>
2020-03-24 22:20:19 -0700

    Deterministic model

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
 
 setuptools.setup(
     packages=setuptools.find_packages(),
     name="seirsplus",
     version='0.0.3',
     description='Models of SEIRS epidemic dynamics with extensions, including network-structured populations, testing, contact tracing, and social distancing.',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
     url="https://github.com/ryansmcgee/SEIRS-network-model",
     author='Ryan Seamus McGee',
     author_email='ryansmcgee@gmail.com',
