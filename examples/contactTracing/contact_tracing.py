a16f6ac46dc35b32d25701fe375c54dca8c4099d
kpelechrinis <kostas.pelechrinis@gmail.com>
2020-04-01 09:57:52 -0400

    Update contact_tracing.py

--- examples/contactTracing/contact_tracing.py
+++ examples/contactTracing/contact_tracing.py
@@ -1,6 +1,8 @@
 from seirsplus.models import *
 import networkx
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 numNodes = 10000
 baseGraph    = networkx.barabasi_albert_graph(n=numNodes, m=9)
@@ -21,23 +23,21 @@ checkpoints = {'t': [20, 100], 'G': [G_distancing, G_normal], 'p': [0.1, 0.5]}
 
 model.run(T=300, checkpoints=checkpoints)
 
-model.figure_infections(title="No contact tracing")
-
 ## add contact tracing
 
 results_I = dict()
 results_F = dict()
-results_Ts = dict(0
+results_Ts = dict()
 
 results_I['no-contact-tracing'] = model.numI
 results_F['no-contact-tracing'] = model.numF
 results_Ts['no-contact-tracing'] = model.tseries
 
-#results.append((0,model.numF[-1],max(model.numI),model.numS[-1]))
-
 # contact tracing 
 
-for phi in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
+PhiS = list(np.linspace(0,1,11))
+
+for phi in PhiS:
   
   #no lag
   model_ct = SEIRSNetworkModel(G=G_normal,beta=0.155, sigma=1/5.2,gamma=1/16.39,mu_I=0.01,p=0.5,
@@ -61,13 +61,11 @@ for phi in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
   results_I['contact-tracing-Lag-phi:'+str(phi)] = model_ct.numI
   results_F['contact-tracing-Lag-phi:'+str(phi)] = model_ct.numF
   results_Ts['contact-tracing-Lag-phi:'+str(phi)] = model_ct.tseries
-  #results.append((phi,model_ct.numF[-1],max(model_ct.numI),model_ct.numS[-1]))
-  #model_ct.figure_infections(title="Contact tracing")
   
-deaths_df = pd.DataFrame(list(zip([0.1,0.2,0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
-            [results_F['contact-tracing-Lag-phi:'+str(i)][-1] for i in [0.1,0.2,0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]],
-            [results_F['contact-tracing-noLag-phi:'+str(i)][-1] for i in [0.1,0.2,0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]])),
-            columns=['phi','Lag','NoLag'],index=[0.1,0.2,0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+deaths_df = pd.DataFrame(list(zip(PhiS,
+            [results_F['contact-tracing-Lag-phi:'+str(i)][-1] for i in PhiS],
+            [results_F['contact-tracing-noLag-phi:'+str(i)][-1] for i in PhiS])),
+            columns=['phi','Lag','NoLag'],index=PhiS)
 
 
 fig, ax = plt.subplots()
@@ -79,15 +77,19 @@ plt.annotate("No contact tracing",(0.45,results_F['no-contact-tracing'][-1]+10))
 # Infections time-series
 
 
-plt.plot(results_Ts['no-contact-tracing'],results_I['no-contact-tracing'],marker='', 
+plt.plot(results_Ts['no-contact-tracing'],results_I['no-contact-tracing']/numNodes,marker='', 
          color='red', linewidth=2, linestyle='dashed', label="No Contact Tracing")
-plt.plot(results_Ts['contact-tracing-noLag-phi:0.1'],results_I['contact-tracing-noLag-phi:0.1'],marker='', 
+plt.plot(results_Ts['contact-tracing-noLag-phi:0.1'],results_I['contact-tracing-noLag-phi:0.1']/numNodes,marker='', 
          color='olive', linewidth=2, linestyle='dashed', label="Contact Tracing (no lag)")
-plt.plot(esults_Ts['contact-tracing-Lag-phi:0.1'],results_I['contact-tracing-Lag-phi:0.1'],marker='', 
+plt.plot(results_Ts['contact-tracing-Lag-phi:0.1'],results_I['contact-tracing-Lag-phi:0.1']/numNodes,marker='', 
          color='blue', linewidth=2, linestyle='dashed', label="Contact Tracing (Lag)")
 
-for phi in [0.2,0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
-  plt.plot(results_Ts['contact-tracing-noLag-phi:'+str(phi)],results_I['contact-tracing-noLag-phi:'+str(phi)],marker='', 
+for phi in Phis[2:11]:
+  plt.plot(results_Ts['contact-tracing-noLag-phi:'+str(phi)],results_I['contact-tracing-noLag-phi:'+str(phi)]/numNodes,marker='', 
          color='olive', linewidth=2, linestyle='dashed')
-  plt.plot(results_Ts['contact-tracing-Lag-phi:'+str(phi)],results_I['contact-tracing-Lag-phi:'+str(phi)],marker='', 
+  plt.plot(results_Ts['contact-tracing-Lag-phi:'+str(phi)],results_I['contact-tracing-Lag-phi:'+str(phi)]/numNodes,marker='', 
          color='blue', linewidth=2, linestyle='dashed')
+  
+plt.legend()
+plt.xlabel("Days")
+plt.ylabel("Fraction of infected population")
