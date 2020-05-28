696b4e25085b583b36c4822616e86f75bf577fd5
Ryan McGee <ryan@Misty.local>
2020-05-28 08:20:47 -0700

    Bug fix in deterministic SEIRSModel

--- examples/models_test.py
+++ examples/models_test.py
@@ -546,7 +546,7 @@ class SEIRSNetworkModel():
         self.parameters = { 'beta':beta, 'sigma':sigma, 'gamma':gamma, 'xi':xi, 'mu_I':mu_I, 'mu_0':mu_0, 'nu':nu, 
                             'beta_D':beta_D, 'sigma_D':sigma_D, 'gamma_D':gamma_D, 'mu_D':mu_D, 
                             'beta_local':beta_local, 'beta_D_local':beta_D_local, 'p':p,'q':q,
-                            'theta_E':theta_E, 'theta_I':theta_I, 'phi_E':phi_E, 'phi_I':phi_I, 'psi_E':psi_E, 'psi_I':psi_I }
+                            'theta_E':theta_E, 'theta_I':theta_I, 'phi_E':phi_E, 'phi_I':phi_I, 'psi_E':phi_E, 'psi_I':psi_I }
         self.update_parameters()
 
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
