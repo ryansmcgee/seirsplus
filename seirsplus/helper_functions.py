9683e037b2eab8666ba7e3ab8bac7a60c945b9e6
jrhomburger <jrh@color.com>
2020-06-24 14:10:22 -0700

    adding helper function to extract daily cases

--- seirsplus/helper_functions.py
+++ seirsplus/helper_functions.py
@@ -2,6 +2,53 @@ import numpy
 
 ###
 
+import sys
+import networkx
+import numpy as np
+import pandas as pd
+
+from extended_models import *
+from networks import *
+from sim_loops import *
+from helper_functions import *
+
+def get_regular_series_output(model, tmax):
+    """
+    Given a seirsplus model, returns a pandas data frame of a regular series (ie per day)
+    of the number of infecteds, infecteds + exposed, and i+e+q.
+    Fills in zero for days past the end of the models
+    """
+    total_i= model.numI_pre + model.numI_sym + model.numI_asym
+    total_i_e = total_i + model.numE
+    total_i_e_q = total_i_e + model.numQ_pre + model.numQ_sym + model.numQ_asym + model.numQ_R
+
+    i_e_series = pd.Series(total_i_e, index=model.tseries)
+
+    # Build the
+    reg_i_e = []
+    reg_i = []
+    reg_i_e_q = []
+    times = []
+    for i in np.arange(0,tmax,1):
+        last_timepoint = np.searchsorted(model.tseries, i)
+        times.append(i)
+        if last_timepoint < len(model.tseries):
+            reg_i_e.append(total_i_e[last_timepoint])
+            reg_i.append(total_i[last_timepoint])
+            reg_i_e_q.append(total_i_e_q[last_timepoint])
+        else:
+            reg_i_e.append(total_i_e[-1])
+            reg_i.append(total_i[-1])
+            reg_i_e_q.append(total_i_e_q[-1])
+
+    return(pd.DataFrame({'total_i':reg_i,
+                        'total_i_q_e':reg_i_e_q,
+                        'total_i_e': reg_i_e,
+                        'time':times,
+                        'seed':model.seed}
+    ))
+
+
 def get_gamma_dist(mean, coeffvar, N):
     scale = mean*coeffvar**2
     shape = mean/scale
@@ -14,8 +61,10 @@ def basic_distributions(N, quarantine_effectiveness=1.0,
     prodromalPeriod_coeffvar = 0.5,
     symptomaticPeriod_mean = 4.0,
     symptomaticPeriod_coeffvar = 0.4,
-    R0_mean = 2.5,
-    R0_coeffvar = 2.0):
+    R0_mean = 2.0,
+    R0_coeffvar = 1.0): # try drawing this from unif(0.2,2), 0.2 comes out with top 20th percentile at 80% of distribution value
+    # 2.0 is close to imperial college distribution, could go a bit higher to tighten the distribution.
+    # Look at if there is a 90/10 value to bound the low end on.
     """
     Builds and returns the basic distributions of the parameters seen in the ipython NB, including
     gamma, lambda, sigma, and betas
