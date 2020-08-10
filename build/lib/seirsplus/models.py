Issue#11 floating point precision bug fix in SEIRSModel run_epoch

build/lib/seirsplus/models.py
@@ -135,7 +135,7 @@ class SEIRSModel():
         t_eval    = numpy.arange(start=self.t, stop=self.t+runtime, step=dt)
 
         # Define the range of time values for the integration:
-        t_span          = (self.t, self.t+runtime)
+        t_span          = [self.t, self.t+runtime]
 
         # Define the initial conditions as the system's current state:
         # (which will be the t=0 condition if this is the first run of this model, 
@@ -149,7 +149,7 @@ class SEIRSModel():
         solution        = scipy.integrate.solve_ivp(lambda t, X: SEIRSModel.system_dfes(t, X, self.beta, self.sigma, self.gamma, self.xi, self.mu_I, self.mu_0, self.nu,
                                                                                             self.beta_Q, self.sigma_Q, self.gamma_Q, self.mu_Q, self.theta_E, self.theta_I, self.psi_E, self.psi_I, self.q
                                                                                         ), 
-                                                        t_span=[self.t, self.tmax], y0=init_cond, t_eval=t_eval
+                                                        t_span=t_span, y0=init_cond, t_eval=t_eval
                                                    )
 
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~