f99218cf340d841d80413bb5677ae5507b93c4bc
Noel Schutt <noel@schutt.org>
2021-03-13 12:34:21 -0500

    Split the models into separate files for ease of extension.

---
+++ seirsplus/models/__init__.py
@@ -0,0 +1,5 @@
+"""SEIRS and extended SEIRS models."""
+
+from .extended_seirs_network_model import ExtSEIRSNetworkModel
+from .seirs_model import SEIRSModel
+from .seirs_network_model import SEIRSNetworkModel
